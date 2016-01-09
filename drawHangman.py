8e519a2536705d4445de124d6c17604240936c62
kritishpahi <kritishpahi@gmail.com>
2016-01-09 10:14:54 +0000

    Draw Hangman

---
+++ drawHangman.py
@@ -0,0 +1,18 @@
+def hangman(trial):
+	if trial == 0:
+		print("""\t  _________
+\t  |
+\t  |""")
+	if trial == 1:
+		print("\t  O")
+	if trial == 2:
+		print("\t \|/")
+	if trial == 3:
+		print("\t  |")
+	if trial == 4:
+		print("\t / \\")
+
+
+
+#for i in range(0,5):
+#	hangman(i)
---
+++ main.py
@@ -0,0 +1,12 @@
+import drawHangman
+import time
+from time import sleep
+import random
+
+
+#These are the key words:
+keys = ["nepal" , "about", "education", "handsome", "country"]
+
+question = random.choice(keys)
+
+print(question)
